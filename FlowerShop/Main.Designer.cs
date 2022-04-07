33c5c58892ce430f9199518a833f1363bd7d39b7
Анастасия Аношина <anoshina050503@gmail.com>
2022-04-07 23:00:24 +0300

    изменение главного окна

--- FlowerShop/Main.Designer.cs
+++ FlowerShop/Main.Designer.cs
@@ -37,54 +37,50 @@ namespace FlowerShop
             // 
             // button6
             // 
-            this.button6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button6.BackColor = System.Drawing.Color.Transparent;
             this.button6.FlatAppearance.BorderSize = 0;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(431, 12);
+            this.button6.Location = new System.Drawing.Point(25, 664);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(39, 35);
+            this.button6.Size = new System.Drawing.Size(251, 135);
             this.button6.TabIndex = 10;
-            this.button6.Text = "в";
             this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.DarkGray;
+            this.button4.BackColor = System.Drawing.Color.Transparent;
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(62, 152);
+            this.button4.Location = new System.Drawing.Point(12, 337);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(177, 34);
+            this.button4.Size = new System.Drawing.Size(241, 34);
             this.button4.TabIndex = 9;
-            this.button4.Text = "журналы";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.DarkGray;
+            this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(62, 192);
+            this.button3.Location = new System.Drawing.Point(12, 505);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(177, 35);
+            this.button3.Size = new System.Drawing.Size(241, 35);
             this.button3.TabIndex = 8;
-            this.button3.Text = "канцтовары";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.DarkGray;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(62, 112);
+            this.button2.Location = new System.Drawing.Point(12, 148);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(177, 34);
+            this.button2.Size = new System.Drawing.Size(140, 34);
             this.button2.TabIndex = 7;
-            this.button2.Text = "книги";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -93,7 +89,8 @@ namespace FlowerShop
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Linen;
-            this.ClientSize = new System.Drawing.Size(482, 254);
+            this.BackgroundImage = global::FlowerShop.Properties.Resources.bookshelf;
+            this.ClientSize = new System.Drawing.Size(590, 878);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
